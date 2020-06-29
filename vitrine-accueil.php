05132b438498010f6742228cc162cc9054a1401e
manon29-lab <67279890+manon29-lab@users.noreply.github.com>
2020-06-29 16:23:20 +0200

    Add files via upload

---
+++ vitrine-accueil.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Document</title>
+        <link rel="stylesheet" href="style/vitrine.css">
+    </head>
+    <body>
+        <div >
+
+<?php $current="accueil"; include ("../header.php") ?>
+
+            <div id="text2">
+                <h2 id="titre2">Accueil</h2>
+            </div>
+
+            <section>
+                <article>
+                    <p id="para">Le numérique, nouvelle ère de l’humanité, source d’innovation et de performance ?
+                        Pour mieux comprendre cette révolution, venez apprendre les rudiments du code et
+                        découvrir les bases de la programmation des sites internet. Destiné aux étudiants,
+                        ce code camp vous initiera en un mois aux principaux langages de programmation
+                        Web (HTML-CSS et PHP), vous dévoilera le fonctionnement d’outils comme Google ou
+                        Paypal et vous éclaira sur la sécurité des sites internet.
+                        </p>
+                </article>
+                <aside>
+                    <img src="epi1.jpg" class="img" alt="epi1">
+                    <img src="epi2.jpg" class="img" alt="epi2">
+                </aside>
+            </section>
+
+            <footer>
+                
+                    <div class="image">
+                        <a href="https://www.epitech.eu/"><img  src="logo_epitech.png" alt="logo"></a>
+                    </div>
+                    
+            </footer>
+        </div>
+    </body>
+</html>
