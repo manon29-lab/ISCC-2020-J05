f6b8fb92786ff1f64622b9fdfd2394dbf91c7695
manon29-lab <67279890+manon29-lab@users.noreply.github.com>
2020-06-29 16:20:47 +0200

    Add files via upload

---
+++ hello-world.php
@@ -0,0 +1,9 @@
+<DOCTYPE html>
+    <html lang- 'fr'>
+    <head>
+    <title> Hello World</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+
+<?php echo '<p> Hello World! </p>'; ?>
